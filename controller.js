11f8a2b3243b71a6b437d6e737cba7ccb0247090
Seth Sutherland <cokolatepal@gmail.com>
2017-04-20 15:36:36 -0600

    everything good

--- controller.js
+++ controller.js
@@ -1,3 +1,9 @@
-angular.module('fluidApp').controller('mainCtrl', function($scope){
+angular.module('fluidApp').controller('mainCtrl', function($scope, $window){
 
+    $scope.fluidInit = function(){
+        $window.FluidEffect.init();
+        $window.FluidEffect.start();
+    }
+
+    $scope.fluidInit();
 });
